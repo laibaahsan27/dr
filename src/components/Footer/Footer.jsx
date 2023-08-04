95ba82710f3e3758f3f134c74b54686028d9e083
laiba ahsan <laiba.ahsan27@gmail.com>
2023-08-04 23:52:37 +0530

    bgg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ import MAIL from '../../images/MailLogo.svg'
 
 export default function Footer() {
   return (
-    <div style={{backgroundColor:'#FFF5F4'}}>
+    <div className='footerOUT'>
       <div className='footerCON'>
         <div className='footerCON1'>
         <div className='footer1'>
