70fb10259d3ab112ee5fdc0e573ff68d2ef321c0
laiba ahsan <laiba.ahsan27@gmail.com>
2023-08-15 21:48:41 +0530

    service pages laiba comp

---
+++ frontend/src/app/service6/page.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './css.css'
+
+export default function page() {
+const list=[
+    {
+        id: 1,
+        heading: 'Anti - Ageing Procedures',
+        content: 'We offer a range of advanced and effective anti-ageing treatments that help you restore youthful vitality and rejuvenate your appearance. Discover the transformative power of our anti-ageing procedures and embark on a journey to renewed confidence and radiance.',    
+    },
+];
+
+  return (
+    <div>
+      {list.map((list) => {
+          return (
+            <div className='outerS' id={list.id}>
+            <div className='topHead'>{list.heading}</div>
+            <div className='topTxt'>{list.content}</div>
+            </div>
+          );
+        })}
+    </div>
+  )
+}
