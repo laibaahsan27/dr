6f2ea8a63fb2146363af543203a3aba1394b8031
PR1YAM9 <priyam9maini@gmail.com>
2023-08-17 21:41:54 +0530

    sanya ayi keheti bale bale

--- frontend/src/components/ServiceCard/ServiceCard.jsx
+++ frontend/src/components/ServiceCard/ServiceCard.jsx
@@ -9,7 +9,7 @@ const ServiceCard = () => {
     <>
       <div className="contactNow">
         <div className="contactNowInner">
-          <div className="contactHeading">Contact Now</div>
+          <div className="contactHeading">CONTACT NOW</div>
 
           <div className="serviceForm">
             <form className="serviceFormInner" action="">
@@ -26,7 +26,7 @@ const ServiceCard = () => {
                 placeholder="Book Service"
               />
               <button className="SFbutton" type="submit">
-                Book an appointment
+                Book an Appointment
               </button>
               <a className="SFconsultation" href="">
                 Or book an Online Consultaion
@@ -35,9 +35,10 @@ const ServiceCard = () => {
           </div>
 
           <div className="location">
-            <div className="contactHeading">Our Location</div>
+            <div className="contactHeading contactLoc">OUR LOCATION</div>
             <div className="maps">
-              <iframe
+              <iframe 
+                className="map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.8427460706107!2d77.1045770741803!3d28.48428209079982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1f3f17329a3d%3A0x7f8a3e8043429deb!2sEHI%20International%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1692267910213!5m2!1sen!2sin"
                 width="372"
                 height="200"
