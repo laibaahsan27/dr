c91ec7893aa59e5e282db06b769df946b455d570
laiba ahsan <laiba.ahsan27@gmail.com>
2023-08-19 16:52:37 +0530

    idk why bhai

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html suppressHydrationWarning={true} lang="en">
+    <html suppressHydrationWarning={true} lang="en" style={{margin:'0', padding: '0', width:'100%'}}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz@6..12&family=Pacifico&family=Poppins&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz@6..12&family=Poppins:wght@200&display=swap" rel="stylesheet"></link>
       </head>
-      <body suppressHydrationWarning={true} className={inter.className} style={{margin:'0', backgroundColor:'#FFF5F4'}}>
+      <body suppressHydrationWarning={true} className={inter.className} style={{margin:'0', backgroundColor:'#FFF5F4',width:'100%'}}>
       <Nav/>
       {children}
       <Foot/>
--- frontend/src/components/ServiceHeading/ServiceHeading.jsx
+++ frontend/src/components/ServiceHeading/ServiceHeading.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./ServiceHeading.css";
 
 const ServiceHeading = ({ heading, content }) => {
   return (
-    <>
-    <div className='outerBg'>
-      <div className="outerS" id={1}>
-        <div className="topHead">{heading}</div>
-        <div className="topTxt">{content}</div>
-      </div>
+    <div className="outerS" id={1}>
+      <div className="topHead">{heading}</div>
+      <div className="topTxt">{content}</div>
     </div>
-    </>
   );
-}
+};
 
 export default ServiceHeading;
