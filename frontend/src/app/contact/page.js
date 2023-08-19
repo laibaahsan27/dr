1d55145e2cca1d9be1ec4dfbc775a61f508b8bc4
PR1YAM9 <priyam9maini@gmail.com>
2023-08-19 13:40:32 +0530

    commit

---
+++ frontend/src/app/contact/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./contact.css";
+import Image from "next/image";
+import clock from "../../images/clock.svg";
+const page = () => {
+  return (
+    <>
+      <div className="contactContainer">
+        <div className="headingCC">
+          <p className="headingContact">Contact</p>
+        </div>
+
+        <div className="leftcard">
+          
+          
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default page;
