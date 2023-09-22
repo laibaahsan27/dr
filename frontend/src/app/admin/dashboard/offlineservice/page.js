a0ffb65856076bfa48ec735df77887c1504e3d75
Priyam Maini <priyam9maini@gmail.com>
2023-09-22 23:28:07 +0530

    search by name added

--- frontend/src/app/admin/dashboard/offlineservice/page.js
+++ frontend/src/app/admin/dashboard/offlineservice/page.js
@@ -6,6 +6,7 @@ import './OflineService.css'; // Import your CSS file
 
 const Page = () => {
   const [serviceRequests, setServiceRequests] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     getServiceRequests();
@@ -21,9 +22,29 @@ const Page = () => {
     }
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Filter service requests based on the search query
+  const filteredServiceRequests = serviceRequests.filter((serviceRequest) =>
+    serviceRequest.fullName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
       <h1 className='ofsHeading'>OFFLINE SERVICE REQUESTS</h1>
+
+      {/* Add the search input field */}
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by Name"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </div>
+
       <table className="styled-table">
         <thead>
           <tr>
@@ -31,18 +52,18 @@ const Page = () => {
             <th>Phone Number</th>
             <th>Email</th>
             <th>Selected Service</th>
-            <th>Date and Time</th> {/* Add a new column */}
+            <th>Date and Time</th>
           </tr>
         </thead>
         <tbody>
-          {serviceRequests.map((serviceRequest) => (
+          {filteredServiceRequests.map((serviceRequest) => (
             <tr key={serviceRequest._id}>
               <td>{serviceRequest.fullName}</td>
               <td>{serviceRequest.phoneNumber}</td>
               <td>{serviceRequest.email}</td>
               <td>{serviceRequest.selectedService}</td>
               <td>
-                {new Date(serviceRequest.createdAt).toLocaleString()} {/* Format date and time */}
+                {new Date(serviceRequest.createdAt).toLocaleString()}
               </td>
             </tr>
           ))}
