9bfe4d6480576827c9955206698d49611aadc351
aadhar2003 <68272092+aadhar2003@users.noreply.github.com>
2023-09-27 16:15:29 +0530

    ok

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -3,12 +3,14 @@ import Nav from "@/components/NavService/NavService";
 import Foot from "@/components/Footer/Footer";
 import ContactCard from "@/components/contactThird/ContactCard";
 import AboutA from "@/components/aboutA/aboutA";
+import AboutB from "@/components/AboutB/AboutB";
 
 export default function page() {
   return (
     <>
       <Nav />
       <AboutA />
+      <AboutB/>
       <ContactCard />
       <Foot />
     </>
---
+++ frontend/src/components/AboutB/AboutB.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './AboutB.css'
+const AboutB = () => {
+  return (
+    <div className='AboutB'>
+        <div className='A-box'>
+            <p>Experience</p>
+        </div>
+        <div className="A-Box2">
+            <div className="Ab-text">
+                <p>1</p>
+              <div className='Ab-text2'>
+                <p>COSMETOLOGIST</p>
+                <p>@Speciality Aesthetics</p>
+              </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutB
